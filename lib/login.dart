6d27c91b689726aef17225b231a91b68e18f7ce8
ChrisShobe <chrisshobe2000@gmail.com>
2024-08-21 20:47:05 -0700

    added firebase

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
-  const LogIn({Key? key}) : super(key: key);
+  const LogIn({super.key});
 
   @override
   Widget build(BuildContext context) {
